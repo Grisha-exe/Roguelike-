1df32fdde91d7ae9cee0e89e47bdc8f979328f69
Gisas <grigorijstekacev@gmail.com>
2025-12-01 18:32:25 +0700

    - feat: boot script created

---
+++ Assets/Project/Scripts/Boot/MainSceneBoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Project.Boot
+{
+    public class MainSceneBoot : MonoBehaviour
+    {
+        private void Start()
+        {
+            Boot();
+        }
+
+        private void Boot()
+        {
+            
+        }
+    }
+}
