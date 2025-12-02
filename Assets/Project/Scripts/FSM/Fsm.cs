3a24478c6b16f2b9e3152d2de04927569d149e9e
Gisas <grigorijstekacev@gmail.com>
2025-12-02 23:43:39 +0700

    - feat: add finite state machine

---
+++ Assets/Project/Scripts/FSM/Fsm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.FSM
+{
+    public class Fsm
+    {
+        public FsmStateBase CurrentState;
+        private Dictionary<Type, FsmStateBase> _states = new();
+
+        public void AddState(FsmStateBase stateBase)
+        {
+            _states.Add(stateBase.GetType(), stateBase);
+        }
+
+        public void SetState<T>() where T : FsmStateBase
+        {
+            var type = typeof(T);
+
+            if (CurrentState?.GetType() == type)
+            {
+                return;
+            }
+
+            if (_states.TryGetValue(type, out var newState))
+            {
+                CurrentState?.Exit();
+
+                CurrentState = newState;
+                
+                CurrentState.Enter();
+            }
+
+        }
+
+        public void Update()
+        {
+            CurrentState?.Update();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/FSM/FsmStateBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Project.FSM
+{
+    public abstract class FsmStateBase
+    {
+        protected readonly Fsm Fsm;
+        protected FsmStateBase CurrentState;
+
+        protected FsmStateBase(Fsm fsm)
+        {
+            Fsm = fsm;
+        }
+        
+        public virtual void Enter() { }
+        
+        public virtual void Update() { }
+        
+        public virtual void Exit() { }
+    }
+}
