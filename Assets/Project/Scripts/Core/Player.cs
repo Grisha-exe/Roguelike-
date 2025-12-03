8149cf314836ea45df7e456ff2c9c266ec99a888
Gisas <grigorijstekacev@gmail.com>
2025-12-03 22:16:37 +0700

    - feat: class player created

---
+++ Assets/Project/Scripts/Core/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace Project.Core
+{
+    public class Player
+    {
+        private int _health;
+        private int _coins;
+        private int _bombs;
+        private int _keys;
+        private int _moveSpeed;
+        private int _damage;
+        private int _fireRate;
+        private int _fireRange;
+        private int _bulletSpeed;
+
+        public void Shoot()
+        {
+            
+        }
+    }
+}
--- Assets/Project/Services/Factories/GlobalFactory.cs
+++ Assets/Project/Services/Factories/GlobalFactory.cs
@@ -1,7 +1,20 @@
-﻿namespace Project.Services.Factories
+﻿using UnityEngine;
+
+namespace Project.Services.Factories
 {
     public class GlobalFactory
     {
-        
+        private GameObject _coreContainer;
+
+        public void Initialize()
+        {
+            
+        }
+
+        public GameObject CreateCoreContainer()
+        {
+            _coreContainer = new GameObject("CoreContainer");
+            return _coreContainer;
+        }
     }
 }
