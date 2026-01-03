8c2fa66751043cfef339920a0fcb4a903a5ff507
Gisas <grigorijstekacev@gmail.com>
2026-01-03 13:43:09 +0700

    - fix: player movement

--- Assets/Project/Scripts/Core/Player.cs
+++ Assets/Project/Scripts/Core/Player.cs
@@ -8,8 +8,13 @@ namespace Project
     public class Player : MonoBehaviour, IMovable
     {
         [Inject] private PlayerInput _playerInput;
+        
+        public float MaxSpeed = 5f;
+        public float Acceleration = 20f;
+        public float Deceleration = 15f;
 
         private Rigidbody2D _rigidbody;
+        private Vector2 _velocity;
         
         public void Update()
         {
@@ -19,11 +24,24 @@ namespace Project
         public void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.linearDamping = 0;
         }
 
         public void Move()
         {
-            _rigidbody.velocity = _playerInput.MoveDirection * 2;
+            Vector2 targetVelocity = _playerInput.MoveDirection * MaxSpeed;
+            if (_playerInput != null)
+            {
+                _velocity = Vector2.MoveTowards(_rigidbody.linearVelocity, 
+                    targetVelocity, Acceleration * Time.deltaTime);
+            }
+            else
+            {
+                _velocity = Vector2.MoveTowards(_rigidbody.linearVelocity,
+                    Vector2.zero, Deceleration * Time.deltaTime);
+            }
+            
+            _rigidbody.linearVelocity = _velocity;
         }
     }
 }
