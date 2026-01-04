8aebb531e2577ade4f56944aecb811cfbf37aaa8
Gisas <grigorijstekacev@gmail.com>
2026-01-04 20:55:00 +0700

    - feat: player animations

--- Assets/Project/Scripts/Core/Player.cs
+++ Assets/Project/Scripts/Core/Player.cs
@@ -9,39 +9,34 @@ namespace Project
     {
         private const float Acceleration = 6.5f;
         private const float Deceleration = 6f;
-        
+
         [Inject] private PlayerInput _playerInput;
-        
+
         public float MaxSpeed = 2f;
+
 // 10,11,15 строчку перенести в playerstats потом.
         private Rigidbody2D _rigidbody;
+        private Animator _animator;
         private Vector2 _velocity;
-        
+
+        public void Awake()
+        {
+            _animator = GetComponentInChildren<Animator>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.linearDamping = 0;
+        }
+
         public void Update()
         {
             Move();
         }
 
-        public void Awake()
-        {
-            _rigidbody = GetComponent<Rigidbody2D>();
-            _rigidbody.linearDamping = 0;
-        }
-
         public void Move()
         {
             Vector2 targetVelocity = _playerInput.MoveDirection * MaxSpeed;
-            if (_playerInput != null)
-            {
-                _velocity = Vector2.MoveTowards(_rigidbody.linearVelocity, 
-                    targetVelocity, Acceleration * Time.deltaTime);
-            }
-            else
-            {
                 _velocity = Vector2.MoveTowards(_rigidbody.linearVelocity,
-                    Vector2.zero, Deceleration * Time.deltaTime);
-            }
-            
+                    targetVelocity, Acceleration * Time.deltaTime);
+                
             _rigidbody.linearVelocity = _velocity;
         }
     }
